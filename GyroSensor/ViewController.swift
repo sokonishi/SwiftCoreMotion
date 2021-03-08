bf04fde1a1465d1eb32c4c34dbb7875751f58a20
SoKonishi <sokonishi315@gmail.com>
2021-03-08 19:44:09 +0900

    Initial Commit

---
+++ GyroSensor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GyroSensor
+//
+//  Created by 小西壮 on 2021/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
